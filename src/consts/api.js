d875e5023aec7d267f8f9efbff1b208b012fdfc8
Gennadiy Rusetskiy <g_rusetskiy@wargaming.net>
2023-11-04 23:04:23 +0200

    Changed push notifications service origin

--- src/consts/api.js
+++ src/consts/api.js
@@ -1,1 +1,1 @@
-export const pushNotificationsServiceOrigin = 'https://ant478-push-notifications.herokuapp.com/api';
+export const pushNotificationsServiceOrigin = 'https://ant478-push-notifications-dd605c846b24.herokuapp.com/api';
